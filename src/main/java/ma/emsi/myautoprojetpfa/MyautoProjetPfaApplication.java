b97c8dde4dc87ba06fea1ccc24cf471780237120
nafidev <nafidev_softdev@gmail.com>
2022-05-17 21:40:24 +0100

    adding CORS filter for data communication with Angular app

--- src/main/java/ma/emsi/myautoprojetpfa/MyautoProjetPfaApplication.java
+++ src/main/java/ma/emsi/myautoprojetpfa/MyautoProjetPfaApplication.java
@@ -1,7 +1,16 @@
 package ma.emsi.myautoprojetpfa;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MyautoProjetPfaApplication {
@@ -9,5 +18,10 @@ public class MyautoProjetPfaApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyautoProjetPfaApplication.class, args);
     }
+
+
+    // If you're using spring security, you should add configuration.setAllowCredentials(true);
+    // here too and don't forget to add http.cors() in spring security configuration.
+
 
 }
